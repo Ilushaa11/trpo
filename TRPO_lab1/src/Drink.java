8b917e93278348d6ac5fe44518b77494436c16e5
Илья Качура <kachura7133453@gmail.com>
2024-04-23 12:47:44 +0300

    Removed TRPO_lab1 from main branch

--- TRPO_lab1/src/Drink.java
+++
@@ -1,8 +0,0 @@
-import java.text.DecimalFormat;
-
-public class Drink extends Dish
-{
-    public Drink(String name, double price, Boolean veganFriedly,double calorieContent) {
-        super(name,price,veganFriedly,calorieContent);
-    }
-}
--- TRPO_lab1/src/Food.java
+++
@@ -1,8 +0,0 @@
-    import java.text.DecimalFormat;
-
-    public class Food extends Dish
-    {
-        public Food(String name, double price, Boolean veganFriedly,double calorieContent) {
-            super(name,price,veganFriedly,calorieContent);
-        }
-    }
