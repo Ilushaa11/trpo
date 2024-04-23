21002a42359aca7f9e00e8fd5ff30dff3440cf2e
Илья Качура <kachura7133453@gmail.com>
2024-04-23 12:16:19 +0300

    Completed lab 1

---
+++ TRPO_lab1/src/Drink.java
@@ -0,0 +1,8 @@
+import java.text.DecimalFormat;
+
+public class Drink extends Dish
+{
+    public Drink(String name, double price, Boolean veganFriedly,double calorieContent) {
+        super(name,price,veganFriedly,calorieContent);
+    }
+}
---
+++ TRPO_lab1/src/Food.java
@@ -0,0 +1,8 @@
+    import java.text.DecimalFormat;
+
+    public class Food extends Dish
+    {
+        public Food(String name, double price, Boolean veganFriedly,double calorieContent) {
+            super(name,price,veganFriedly,calorieContent);
+        }
+    }
